d40dec14c75454d09dbcf5b4ca6ae0bdddfe4544
Ajinkya Prakash Dhomne <ajinkya.10730701@ltimindtree.com>
2023-12-21 10:52:37 +0530

    added helmet middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import helmet from "helmet";
 import { createConnection } from "./db/mongoConnection";
 import blogRouter from "./routers/blog.router";
 
 const init = async () => {
 	const app = express();
+	app.use(helmet());
 	createConnection();
 	app.use("/api/v1", blogRouter);
 	app.listen(3000, () => {
