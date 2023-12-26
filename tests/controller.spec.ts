09b377bb7a84786a1180a34373504c448111bb11
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2023-12-27 00:44:30 +0530

    enhancement: added mocha testing framework configuration

---
+++ tests/controller.spec.ts
@@ -0,0 +1,8 @@
+import { getAllBlogsController } from "../src/controllers/blogs.controller";
+
+describe("Test suite for getAllBlogsController", () => {
+	it("Should return list of blogs", (done) => {
+		// TODO
+		done();
+	});
+});
